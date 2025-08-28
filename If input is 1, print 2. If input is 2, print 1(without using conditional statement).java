72ae7de7e512e28df35ed16c853eb39fa8a510ee
Churchil007 <churchil14052004@gmail.com>
2025-08-28 21:15:01 +0530

    Create If input is 1, print 2. If input is 2, print 1(without using conditional statement).java

---
+++ If input is 1, print 2. If input is 2, print 1(without using conditional statement).java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int val=n+1/n;
+        System.out.println(3-n);
+    }
+}
