25aa9fb9c80b86a93891ab7f2b053719a1338037
Churchil007 <churchil14052004@gmail.com>
2025-08-28 21:11:36 +0530

    Create Swap numbers with a temporary variable.java

---
+++ Swap numbers with a temporary variable.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        a=a*b;
+        b=a/b;
+        a=a/b;
+        System.out.println("num1="+a);
+        System.out.println("num2="+b);
+    }
+}
