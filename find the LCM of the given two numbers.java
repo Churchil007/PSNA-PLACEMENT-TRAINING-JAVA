86c49f705cb9ab8d0b095f83c30657a26df143fd
Churchil007 <churchil14052004@gmail.com>
2025-08-28 20:58:36 +0530

    Create find the LCM of the given two numbers.java

---
+++ find the LCM of the given two numbers.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int max=0;
+        if(a>b){
+            max=a;
+        }
+        else{
+            max=b;
+        }
+        while(true){
+            if(max%a==0 && max%b==0){
+                break;
+            }
+            max++;
+        }
+        System.out.println(max);
+    }
+}
