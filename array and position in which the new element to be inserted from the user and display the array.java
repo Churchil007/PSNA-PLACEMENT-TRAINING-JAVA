466163356e044ab26f5762d2d77c4155c5d2ed98
Churchil007 <churchil14052004@gmail.com>
2025-08-28 19:24:47 +0530

    Create  array and position in which the new element to be inserted from the user and display the array.java

---
+++ array and position in which the new element to be inserted from the user and display the array.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n+1];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int val=sc.nextInt();
+        int pos=sc.nextInt();
+        for(int i=n;i>=pos;i--){
+            arr[i]=arr[i-1];
+        }
+        arr[pos-1]=val;
+        for(int i=0;i<n+1;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
